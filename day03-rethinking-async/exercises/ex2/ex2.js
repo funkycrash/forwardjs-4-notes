33bd6dc69796449f97e76e079228bee1925839c6
nicolas <nicolas.funke@amplifierstrategies.com>
2016-02-11 11:28:57 -0800

    content

--- day03-rethinking-async/exercises/ex2/ex2.js
+++ day03-rethinking-async/exercises/ex2/ex2.js
@@ -27,8 +27,25 @@ function getFile(file) {
 		else resp(text);
 	});
 
-	
+	return function thunk(cb){
+		if (resp) cb(resp);
+		else resp = cb;
+	};
 }
 
+var thunk1 = getFile("file1");
+var thunk2 = getFile("file2");
+var thunk3 = getFile("file3");
+
+
+thunk1(function ready(text){
+	output(text);
+	thunk2(function ready(text) {
+		output(text);
+		thunk3(function ready(text) {
+			
+		})
+	});
+});
 // request all files at once in "parallel"
 // ???
