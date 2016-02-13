835109e3115cf02ecb37dfa2e9499928de76ffa4
nicolas <nicolas.funke@amplifierstrategies.com>
2016-02-13 14:41:17 -0800

    notes

--- day05-deep-dive-into-angular-2/angular-app/app/app.component.js
+++ day05-deep-dive-into-angular-2/angular-app/app/app.component.js
@@ -25,7 +25,7 @@ System.register(['angular2/core'], function(exports_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n\t<ul class=\"heroes\">\n\t  <li *ngFor=\"#hero of heroes\" (click)=\"onSelect(hero)\">\n\t  \t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t  </li>\n\t</ul>\n  <div *ngIf=\"selectedHero\">\n    <h2>{{selectedHero.name}} details!</h2>\n    <div><label>id: </label>{{selectedHero.id}}</div>\n    <div><input [(ngModel)]=selectedHero.name placeholder=\"name\"/></div>\n    </div>\n\t"
+                        template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n\t<ul class=\"heroes\">\n\t  <li *ngFor=\"#hero of heroes\" \n    (click)=\"onSelect(hero)\" \n    [class.selected]=\"hero === selectedHero\">\n\t  \t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t  </li>\n\t</ul>\n  <div *ngIf=\"selectedHero\">\n    <h2>{{selectedHero.name}} details!</h2>\n    <div><label>id: </label>{{selectedHero.id}}</div>\n    <div><input [(ngModel)]=selectedHero.name placeholder=\"name\"/></div>\n    </div>\n\t"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
