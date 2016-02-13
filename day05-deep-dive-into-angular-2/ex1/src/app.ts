d6f0c636d269d6c429a39b15f967d0917bdae05d
nicolas <nicolas.funke@amplifierstrategies.com>
2016-02-13 12:45:59 -0800

    notes

--- day05-deep-dive-into-angular-2/ex1/src/app.ts
+++ day05-deep-dive-into-angular-2/ex1/src/app.ts
@@ -9,11 +9,21 @@ import {Component} from 'angular2/core'
     <div>
       <h2>Hello {{name}}</h2>
       <button (click)="toggleMyName()">Toggle Name</button>
-      
+      <div class=".form"></div>
     </div>
   `,
   directives: []
 })
+
+@Component({
+  selector: 'my-form',
+  providers: [],
+  template: `
+
+  `,
+  directives: []
+})
+
 
 export class App {
   constructor() {
@@ -25,6 +35,19 @@ export class App {
     } else { 
       this.name = 'Nicolas'
     }
+  }
+}
+
+export class LoginPage {
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+  doLogin(event) {
+    console.log(this.loginForm.value);
+    event.preventDefault();
   }
 }
 
