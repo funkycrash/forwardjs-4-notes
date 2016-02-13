73f49557941830985b4923f4a54cf3c022d6e4da
nicolas <nicolas.funke@amplifierstrategies.com>
2016-02-13 15:05:27 -0800

    notes

--- day05-deep-dive-into-angular-2/angular-app/app/hero.service.ts
+++ day05-deep-dive-into-angular-2/angular-app/app/hero.service.ts
@@ -1,6 +1,11 @@
+import {HEROES} from './mock-heroes';
 import {Injectable} from 'angular2/core';
 
 
 @Injectable()
 export class HeroService {
+	getHeroes(){
+		console.log('getting heroes...');
+		return HEROES;		
+	}
 }
