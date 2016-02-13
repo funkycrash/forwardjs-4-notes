d5c8ecebc5ed1ea99c026d095e6bc0e87c4280d1
nicolas <nicolas.funke@amplifierstrategies.com>
2016-02-13 13:04:21 -0800

    notes

--- day05-deep-dive-into-angular-2/ex1/src/app.ts
+++ day05-deep-dive-into-angular-2/ex1/src/app.ts
@@ -3,20 +3,18 @@ import {Component, FormBuilder, Validators} from 'angular2/core'
 @Component({
   selector: 'my-form',
   template: `
-    <form [ngFormModel]="loginForm" (submit)="doLogin($event)">
-    <input ngControl="email" type="email" placeholder="Your email">
-    <input ngControl="password" type="password" placeholder="Your password">
-  <button type="submit">Log in</button>
-</form>
+    <form [ngFormModel]="loginForm" (submit)="doLogin()">
+      <input ngControl="email" type="email" placeholder="Your email">
+      <input ngControl="password" type="password" placeholder="Your password">
+    <button type="submit">Log in</button>
+  </form>
   `
 })
 
 
 export class LoginPage {
-  constructor(fb: FormBuilder) {
-    this.loginForm = fb.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required]
-    });
+  
+  doLogin(){
+    console.log('log in');
   }
 }
